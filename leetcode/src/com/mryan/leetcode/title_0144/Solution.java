a1a87653697cfdf613cc4ed7a454235c1711b1a5
yanmancang <772290288@qq.com>
2020-04-30 00:07:15 +0800

    [描述：]利用数组进行递归求解LeetCode_144(设计链表)

---
+++ leetcode/src/com/mryan/leetcode/title_0144/Solution.java
@@ -0,0 +1,56 @@
+package com.mryan.leetcode.title_0144;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    /**
+     * 给定一个二叉树，返回它的 前序 遍历。
+     * <p>
+     *  示例:
+     * <p>
+     * 输入: [1,null,2,3]
+     * 1
+     * \
+     * 2
+     * /
+     * 3
+     * <p>
+     * 输出: [1,2,3]
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/binary-tree-preorder-traversal
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    List<Integer> list = new ArrayList<>();
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        if (root == null) {
+            return list;
+        }
+        list.add(root.val);
+        preorderTraversal(root.left);
+        preorderTraversal(root.right);
+        return list;
+    }
+
+    public static void main(String[] args) {
+        TreeNode treeNode = new TreeNode(1);
+        TreeNode treeNode1 = new TreeNode(2);
+        TreeNode treeNode2 = new TreeNode(3);
+        treeNode.right = treeNode1;
+        treeNode1.left = treeNode2;
+        System.out.println(new Solution().preorderTraversal(treeNode).toString());
+
+    }
+}
