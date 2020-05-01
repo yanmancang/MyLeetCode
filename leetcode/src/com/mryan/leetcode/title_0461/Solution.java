3efd05181352822721544a3583c0f669907d74c5
yanmancang <772290288@qq.com>
2020-05-01 22:32:52 +0800

    [描述：]利用JavaAPI求解LeetCode_461(汉明距离)

--- leetcode/src/com/mryan/leetcode/title_0461/Solution.java
+++ leetcode/src/com/mryan/leetcode/title_0461/Solution.java
@@ -3,6 +3,7 @@ package com.mryan.leetcode.title_0461;
 import java.util.Map;
 
 public class Solution {
+    // 常规思路，暴力解决
     public int hammingDistance(int x, int y) {
         StringBuilder xStringBuilder = new StringBuilder();
         StringBuilder yStringBuilder = new StringBuilder();
@@ -47,7 +48,40 @@ public class Solution {
         return count;
     }
 
+    // 利用API求解
+    public int hammingDistance(int x, int y, boolean isApi) {
+        StringBuilder newX = new StringBuilder(Integer.toBinaryString(x)).reverse();
+        StringBuilder newY = new StringBuilder(Integer.toBinaryString(y)).reverse();
+        int count = 0;
+        int max = Math.max(newX.length(), newY.length());
+        int xIn, yIn = 0;
+        for (int i = 0; i < max; i++) {
+            if (i >= newX.length()) {
+                xIn = '0';
+            } else {
+                xIn = newX.charAt(i);
+            }
+            if (i >= newY.length()) {
+                yIn = '0';
+            } else {
+                yIn = newY.charAt(i);
+            }
+            if (xIn == yIn) {
+                continue;
+            } else {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    // 按照异或利用API求解
+    public int hammingDistance(int x, int y, boolean isApi, boolean isOk) {
+        return Integer.bitCount(x ^ y);
+    }
+
+
     public static void main(String[] args) {
-        System.out.println(new Solution().hammingDistance(4, 14));
+        System.out.println(new Solution().hammingDistance(1, 4, true, false));
     }
 }
