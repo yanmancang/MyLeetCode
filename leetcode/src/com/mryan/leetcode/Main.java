0629a5d4baa9c1a4bef05f80c577941707300c9f
yanmancang <772290288@qq.com>
2020-05-02 00:36:21 +0800

    [描述：]利用常规求解，针对超时，将特殊数据进行拆分处理LeetCode_0901(股票价格跨度)

--- leetcode/src/com/mryan/leetcode/Main.java
+++ leetcode/src/com/mryan/leetcode/Main.java
@@ -1,8 +1,15 @@
 package com.mryan.leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        List<String> list = new ArrayList<>();
+        list.add("123456");
+        list.add("123456");
+        System.out.println(list.toString());
     }
 }
---
+++ leetcode/src/com/mryan/leetcode/title_901/StockSpanner.java
@@ -0,0 +1,46 @@
+package com.mryan.leetcode.title_901;
+
+import java.util.*;
+
+public class StockSpanner {
+    List<Integer> list = new ArrayList<>();
+    int last = 1;
+
+    public StockSpanner() {
+
+    }
+
+    public int next(int price) {
+        // 去除特殊形况，当前一个数比这次的大，直接返回count=1；
+        if (list.size() == 0 || list.get(list.size() - 1) > price) {
+            list.add(price);
+            last = 1;
+            return last;
+        }
+        // 当相邻两个数相同,则直接返回上一个数的count+1；
+        if (list.get(list.size() - 1) == price) {
+            last = last + 1;
+            list.add(price);
+            return last;
+        }
+        int count = 1;
+        for (int i = list.size() - 1; i >= 0; i--) {
+            if (list.get(i) > price) {
+                break;
+            } else {
+                count++;
+            }
+        }
+        list.add(price);
+        last = count;
+        return count;
+    }
+
+    public static void main(String[] args) {
+        StockSpanner stockSpanner = new StockSpanner();
+        System.out.println(stockSpanner.next(90));
+        System.out.println(stockSpanner.next(28));
+        System.out.println(stockSpanner.next(28));
+        System.out.println(stockSpanner.next(68));
+    }
+}
